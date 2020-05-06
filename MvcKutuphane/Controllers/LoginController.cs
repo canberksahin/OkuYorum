e94e9f0ef73ffc93f5e98ab7bc63898849f2aeeb
Canberk Sahin <canberksahin20@hotmail.com>
2020-05-07 02:57:25 +0300

    Login tamamlandı

---
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using MvcKutuphane.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcKutuphane.Controllers
+{
+    public class LoginController : BaseController
+    {
+        // GET: Login
+        public ActionResult GirisYap()
+        {
+            return View();
+        }
+        [HttpPost,ValidateAntiForgeryToken]
+        public ActionResult GirisYap(Uyeler uye)
+        {
+            var bilgiler = db.Uyeler.FirstOrDefault(x => x.Mail == uye.Mail && x.Sifre == uye.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Mail, false);
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
