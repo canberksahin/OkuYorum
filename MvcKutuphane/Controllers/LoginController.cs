ee6d33194d2d6cbbb0aa920b7b4af005ae5a23c9
Canberk Sahin <canberksahin20@hotmail.com>
2020-05-13 03:06:18 +0300

    Profil Bilgileri

--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -18,10 +19,17 @@ namespace MvcKutuphane.Controllers
         [HttpPost,ValidateAntiForgeryToken]
         public ActionResult GirisYap(Uyeler uye)
         {
-            var bilgiler = db.Uyeler.FirstOrDefault(x => x.Mail == uye.Mail && x.Sifre == uye.Sifre);
+            var bilgiler = db.Uyeler.FirstOrDefault(x => x.Mail == uye.Mail && x.Sifre== uye.Sifre);
             if (bilgiler != null)
             {
                 FormsAuthentication.SetAuthCookie(bilgiler.Mail, false);
+                Session["Mail"] = bilgiler.Mail.ToString();
+                //TempData["Ad"] = bilgiler.Ad.ToString();
+                //TempData["Soyad"] = bilgiler.Soyad.ToString();
+                //TempData["KullaniciAdi"] = bilgiler.KullaniciAdi.ToString();
+                //TempData["Sifre"] = bilgiler.Sifre.ToString();
+                //TempData["Okul"] = bilgiler.Okul.ToString();
+                //TempData["Id"] = bilgiler.Id.ToString();
                 return RedirectToAction("Index", "Panelim");
             }
             else
--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,13 +7,36 @@ using System.Web.Mvc;
 
 namespace MvcKutuphane.Controllers
 {
-    public class PanelimController : Controller
+    public class PanelimController : BaseController
     {
         // GET: Panelim
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var uyeMail = (string)Session["Mail"];
+            var degerler = db.Uyeler.FirstOrDefault(x => x.Mail == uyeMail);
+            return View(degerler);
+        }
+        public ActionResult ProfilGuncelle()
+        {
+            var uyeMail = (string)Session["Mail"];
+            var degerler = db.Uyeler.FirstOrDefault(x => x.Mail == uyeMail);
+            return View(degerler);
+        }
+
+        [HttpPost]
+        public ActionResult ProfilGuncelle(Uyeler uye)
+        {
+            var uyeMail = (string)Session["Mail"];
+            var uyeDb = db.Uyeler.FirstOrDefault(x => x.Mail == uyeMail);
+
+            uyeDb.Ad = uye.Ad;
+            uyeDb.Soyad = uye.Soyad;
+            uyeDb.Sifre = uye.Sifre;
+            uyeDb.KullaniciAdi = uye.KullaniciAdi;
+            uyeDb.Okul = uye.Okul;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
